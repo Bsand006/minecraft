e45a86691c200a8a487959fcd64d2866ebd35387
bran <bran@fedora>
2023-11-05 21:21:19 -0500

    first comit

---
+++ forge-1.20.1-47.2.0-mdk/src/main/java/mod/briansand/llama/Llama.java
@@ -0,0 +1,28 @@
+package mod.briansand.llama;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("llamamod")
+public class Llama {
+	  
+	private static final Logger LOGGER = LogManager.getLogger(); // Initialize logger
+
+	    public Llama() { // Constructor 
+	        LOGGER.info("Creating the mod...");
+	    }
+	    
+	    // Declare event bus subscriber
+	    @Mod.EventBusSubscriber(modid = "llamamod", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+	    public static class RegistryEvents {
+	        @SubscribeEvent
+	        public static void onBlockBreak(BlockEvent.BreakEvent event) { // subscribes to block breaking event bus
+	            LlamaEvents.onBlockBreak(event); // run onBlockBreak when event is triggered
+	        }
+	    }
+
+}
