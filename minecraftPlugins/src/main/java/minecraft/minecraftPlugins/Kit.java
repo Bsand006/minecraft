12e912159086f7a29199445888caf693613506cd
msand <msand@wifi-roaming-128-4-144-19.wifi.udel.edu>
2024-10-14 18:55:24 -0400

    setting up plugins with sql

--- minecraftPlugins/src/main/java/minecraft/minecraftPlugins/Kit.java
+++ minecraftPlugins/src/main/java/minecraft/minecraftPlugins/Kit.java
@@ -1,4 +1,4 @@
-package minecraft.minecraftPlugins;
+  package minecraft.minecraftPlugins;
 
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -7,11 +7,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-public class Plugin implements CommandExecutor {
+public class Kit implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+		
+		
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			ItemStack birch = new ItemStack(Material.BIRCH_LOG);
--- minecraftPlugins/src/main/java/minecraft/minecraftPlugins/Main.java
+++ minecraftPlugins/src/main/java/minecraft/minecraftPlugins/Main.java
@@ -1,17 +1,32 @@
 package minecraft.minecraftPlugins;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+	List<String> banned = new ArrayList<String>();
+	
 	// Enables and initializes the /command plugin
-
 	@Override
 	public void onEnable() {
-		this.getCommand("kit").setExecutor(new Plugin());
+		this.getCommand("kit").setExecutor(new Kit());
+		
 
 		register();
+	}
+	
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		Player p = event.getPlayer();
+		
+		
 	}
 
 	// Register the plugin manager and register OnJoin as an event to listen for
